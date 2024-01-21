8cd68c62a6e0db231fcd7ad69877c30501913b27
Мax <maxlepinskih@gmail.com>
2024-01-21 09:46:32 +0500

    refactoring

--- app/src/main/java/com/example/doubletapp/SquareActivity.kt
+++ app/src/main/java/com/example/doubletapp/SquareActivity.kt
@@ -3,11 +3,11 @@ package com.example.doubletapp
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.doubletapp.constance.Constance
 import com.example.doubletapp.databinding.ActivitySquareBinding
 
 class SquareActivity : AppCompatActivity() {
-    lateinit var binding: ActivitySquareBinding
+    private lateinit var binding: ActivitySquareBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySquareBinding.inflate(layoutInflater)
@@ -21,44 +21,36 @@ class SquareActivity : AppCompatActivity() {
     }
 
     private fun onClickBtnBackToMainAct() {
-        binding.btnToMain.setOnClickListener {
-            finish()
-        }
+        binding.btnToMain.setOnClickListener { finish() }
     }
 
     override fun onResume() {
         super.onResume()
-
         Log.d("MyLog", "onResumeSquareAct")
     }
 
     override fun onPause() {
         super.onPause()
-
         Log.d("MyLog", "onPauseSquareAct")
     }
 
     override fun onStart() {
         super.onStart()
-
         Log.d("MyLog", "onStartSquareAct")
     }
 
     override fun onStop() {
         super.onStop()
-
         Log.d("MyLog", "onStopSquareAct")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-
         Log.d("MyLog", "onDestroySquareAct")
     }
 
     override fun onRestart() {
         super.onRestart()
-
         Log.d("MyLog", "onRestartSquareAct")
     }
 }
